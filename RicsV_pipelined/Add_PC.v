414a3accbb36409c0185f76fd5493e3e36ae7e8a
Thành Đô <148622167+ThanhDo288@users.noreply.github.com>
2024-05-31 22:13:45 +0700

    Delete RicsV_pipelined directory

--- RicsV_pipelined/Add_PC.v
+++
@@ -1,8 +0,0 @@
-///////////////////////////// Add- PC //////////////////////
-module Add_PC(
-    input [31:0] PC_E,
-    input [31:0] Extimm_E,
-    output [31:0] PCSum_out
-);
-    assign PCSum_out= PC_E+Extimm_E;
-endmodule
