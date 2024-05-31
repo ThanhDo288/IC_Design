40f28d7e79754f2a8ac7c0e175104593605c0e00
Thành Đô <148622167+ThanhDo288@users.noreply.github.com>
2024-05-31 22:09:02 +0700

    Delete RicsV_pipelined directory

--- RicsV_pipelined/Mux_Register.v
+++
@@ -1,8 +0,0 @@
-module Mux_Register(
-    input [31:0] Read_data2,
-    input [31:0] ImmExt,
-    input ALUSrc,
-    output [31:0] Mux_Register_out
-);
-assign Mux_Register_out= (ALUSrc)? ImmExt: Read_data2;
-endmodule
